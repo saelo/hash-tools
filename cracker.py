9e976399304a9019f427dd453a975aa403825f52
Samuel Groß <dev@samuel-gross.de>
2012-12-13 22:21:41 +0000

    Fixed memory issue.

--- cracker.py
+++ cracker.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+#
+# simple hash cracker using multiprocessing
+# 
 
-import hashlib
-from multiprocessing import Pool
+from hashlib import sha1
+from multiprocessing import Pool, Array
 
 #
 # config
@@ -12,52 +15,79 @@ HASHFILE = ""	# must be in format hash:salt
 #
 # global vars
 #
-words = []
-hashes = []
+words = None
 
-#
-# helper functions
-#
-def genHash(salt, passwd):
-	return mkhash(salt + mkhash(salt + mkhash(passwd)))
-
-def mkhash(value):
-	return hashlib.sha1(value).hexdigest()
-
-def crack(hstuple):
-	global words, results
-	hashv = hstuple[0] 
-	salt = hstuple[1]
-	for word in words:
-		if hashv == genHash(salt, word.strip()).lower():
-			print("Found %s for %s" % (word.strip(), hashv))
-			break
+'''
+ hash function
+ override as needed
+'''
+def gethash(salt, passwd):
+	return sha1(salt + sha1(salt + sha1(passwd).hexdigest()).hexdigest()).hexdigest()
 
 
+'''
+ try to crack given tuple of hash and salt
+'''
+def crack(hashv, salt):
+	global words
+	word = ""
+	for char in words:
+		if char == "\n":
+			if hashv == gethash(salt, word.strip()).lower():
+				print("[!] found %s for %s" % (word.strip(), hashv))
+				return (word.strip(), hashv)
+			word = ""
+			
+		word += char
 
+'''
+ thread entry point
+ args:
+ [0] : hash
+ [1] : salt
+'''
+def entry(args):
+	hashv = args[0]
+	salt = args[1]
+
+	return crack(hashv, salt)
+
+
+
+
+'''
+ main - reads input files and manages worker threads
+'''
 def main():
 	#
 	# open files
 	#
-	global WORDLIST, HASHFILE, words, hashes, result
+	print("[*] parsing wordlist...")
+	global WORDLIST, HASHFILE, words, result
 	wordlist = open(WORDLIST, 'r')
-	words = wordlist.read().split('\n')
+	words = Array('c', wordlist.read(), lock=False)
 	wordlist.close()
+	print("[*] reading hashes...")
 	hashes = open(HASHFILE, 'r').read().split("\n")
+	hashlist = []
 
 	#
 	# crack
 	#
-	hstuplelist = []
+	print("[*] beginning cracking")
+	pool = Pool(processes=8)
+
 	for hash in hashes:
 		data = hash.split(":")
 		if len(data) > 1:
 			hashv = data[0].strip()
 			salt = data[1].strip()
-			hstuplelist.append((hashv, salt))
+			hashlist.append((hashv, salt))
 
-	pool = Pool(processes=8, maxtasksperchild=1)
-	pool.map(crack, hstuplelist)
+
+	pool.map(entry, hashlist)
+
+	print("[*] shutting down")
 
 
 if __name__ == "__main__":
