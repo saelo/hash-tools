48aa15b6786568836af1af8294789af94496a0b5
Samuel Groß <dev@samuel-gross.de>
2012-12-15 14:10:28 +0000

    Some code improvements.

--- cracker.py
+++ cracker.py
@@ -9,15 +9,18 @@ from multiprocessing import Pool, Array, Value
 #
 # config
 #
-WORDLIST = "./cracklist.txt"
-HASHFILE = "./hashes.txt"	# must be in format hash:salt
+WORDLIST         = "./cracklist.txt"
+HASHFILE         = "./hashes.txt"	# must be in format hash:salt
+NUM_PROCESSES    = 8
+PERC_GRANULARITY = 50000	# number of processed words after which to update percentage
 
 #
 # global vars
 #
-words = None	# shared memory segment containing the wordlist
-curr  = None	# shared variable containing the current total count of processed words and hashes
-total = None	# shared variable containing the total count of words times hashes
+words     = None	# shared memory segment containing the wordlist
+num_words = 0
+curr      = None	# shared variable containing the current total count of processed words and hashes
+total     = None	# shared variable containing the total count of words times hashes
 
 
 '''
@@ -32,24 +35,29 @@ def gethash(salt, passwd):
  try to crack given tuple of hash and salt
 '''
 def crack(hashv, salt):
-	global words, curr, total
+	global PERC_GRANULARITY, words, curr, total
 	count = 0
 	word = ""
+	res = None
+
 	for char in words:
 		if char == "\n":
 			word = word.strip()
 			count += 1
-			if count % 100000 == 0:
-				curr.value += 100000
+			if count % PERC_GRANULARITY == 0:
+				curr.value += PERC_GRANULARITY
 				print("[%%] %f" % ((curr.value / total.value) * 100))
 			if hashv == gethash(salt, word).lower():
-				print("[!] found %s for %s" % (word, hashv))
-				return (word, hashv)
+				res = (word, hashv)
+				print("[!] found %s for %s" % res)
+				break		# done
 			word = ""
 			
 		word += char
-
-	return None
+	
+	curr.value += (num_words - count) if count != num_words else count % PERC_GRANULARITY	 	# add reminder to current value
+	print("[%%] %f" % ((curr.value / total.value) * 100))
+	return res
 
 '''
  thread entry point
@@ -64,7 +72,6 @@ def entry(args):
 	return crack(hashv, salt)
 
 
-
 '''
  main - reads input files and manages worker threads
 '''
@@ -73,38 +80,44 @@ def main():
 	# process files
 	#
 	print("[*] parsing wordlist...")
-	global WORDLIST, HASHFILE, words, result, curr, total
-	wordlist = open(WORDLIST, 'r')
-	wordsstr = wordlist.read()
+	global WORDLIST, HASHFILE, words, result, curr, total, num_words
+	wordlist_file = open(WORDLIST, 'r')
+	wordlist = wordlist_file.read()
+	num_words = len(wordlist.split("\n"))
 	# load wordlist in shared memory segment
-	words = Array('c', wordsstr, lock=False)
+	words = Array('c', wordlist, lock=False)
 
 	print("[*] reading hashes...")
-	hashes = open(HASHFILE, 'r').read().split("\n")
+	hashes = open(HASHFILE, 'r')
 	hashlist = []
-	for hash in hashes:
-		data = hash.split(":")
+	for line in hashes:
+		data = line.split(":")
 		if len(data) > 1:
 			hashv = data[0].strip()
 			salt = data[1].strip()
 			hashlist.append((hashv, salt))
 
-	total = Value('i', len(wordsstr.split("\n")) * len(hashlist))
+	total = Value('i', num_words * len(hashlist))
 	curr = Value('d', 0.0)
 
-	wordlist.close()
+	# 
+	# free resources
+	#
+	wordlist_file.close()
+	del wordlist		# takes up quite a bit of memory, so lets free it
+	hashes.close()
 
 	#
 	# crack
 	#
 	print("[*] beginning cracking")
-	pool = Pool(processes=8)
+	pool = Pool(processes=NUM_PROCESSES)
 	results = pool.map(entry, hashlist)
 
 	print("[*] done")
 
 	for result in results:
-		if result not None:
+		if result is not None:
 			print("%s:%s" % (result))
 
 
