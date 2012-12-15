bbac3ea3b57a1e8140b60db48ed76614703af55a
Samuel Groß <dev@samuel-gross.de>
2012-12-15 12:41:42 +0000

    Added percentage display.

--- cracker.py
+++ cracker.py
@@ -4,18 +4,20 @@
 # 
 
 from hashlib import sha1
-from multiprocessing import Pool, Array
+from multiprocessing import Pool, Array, Value
 
 #
 # config
 #
-WORDLIST = ""
-HASHFILE = ""	# must be in format hash:salt
+WORDLIST = "./cracklist.txt"
+HASHFILE = "./hashes.txt"	# must be in format hash:salt
 
 #
 # global vars
 #
 words = None	# shared memory segment containing the wordlist
+curr  = None	# shared variable containing the current total count of processed words and hashes
+total = None	# shared variable containing the total count of words times hashes
 
 
 '''
@@ -30,10 +32,15 @@ def gethash(salt, passwd):
  try to crack given tuple of hash and salt
 '''
 def crack(hashv, salt):
-	global words
+	global words, curr, total
+	count = 0
 	word = ""
 	for char in words:
 		if char == "\n":
+			count += 1
+			if count % 100000 == 0:
+				curr.value += 100000
+				print("[%] %f" % (curr.value / total.value))
 			if hashv == gethash(salt, word.strip()).lower():
 				print("[!] found %s for %s" % (word.strip(), hashv))
 				return (word.strip(), hashv)
@@ -63,11 +70,11 @@ def main():
 	# process files
 	#
 	print("[*] parsing wordlist...")
-	global WORDLIST, HASHFILE, words, result
+	global WORDLIST, HASHFILE, words, result, curr, total
 	wordlist = open(WORDLIST, 'r')
+	wordsstr = wordlist.read()
 	# load wordlist in shared memory segment
-	words = Array('c', wordlist.read(), lock=False)
-	wordlist.close()
+	words = Array('c', wordsstr, lock=False)
 
 	print("[*] reading hashes...")
 	hashes = open(HASHFILE, 'r').read().split("\n")
@@ -78,6 +85,11 @@ def main():
 			hashv = data[0].strip()
 			salt = data[1].strip()
 			hashlist.append((hashv, salt))
+
+	total = Value('i', len(wordsstr.split("\n")))
+	curr = Value('d', 0.0)
+
+	wordlist.close()
 
 	#
 	# crack
